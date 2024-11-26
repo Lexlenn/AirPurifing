d88d9b2b28dad36bc81d9e5d10c19f383a6b255c
leonardo brandão <twobrothers.leo.gui@gmail.com>
2024-11-26 13:29:51 -0300

    yes

--- Index.js
+++ Index.js
@@ -3,4 +3,11 @@ document.addEventListener('DOMContentLoaded', function(){
     var altura = document.getElementById('navBar').clientHeight
     document.getElementById('lay1').style.height = document.getElementById('hiper').clientHeight - altura + "px"
     document.getElementById('lay1').style.top = `${altura}px`
+
+    window.addEventListener('resize', function() {
+    document.getElementById('lay1').style.height = document.getElementById('hiper').clientHeight - altura + "px"
+    document.getElementById('lay1').style.top = `${altura}px`
+    });
 })
+
+
