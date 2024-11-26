8cf31421a057ee45fbd3c71c3be8b15cdd05d0e9
leonardo brandão <twobrothers.leo.gui@gmail.com>
2024-11-26 12:48:17 -0300

    site

---
+++ Index.js
@@ -0,0 +1,6 @@
+// JavaScript source code
+document.addEventListener('DOMContentLoaded', function(){
+    var altura = document.getElementById('navBar').clientHeight
+    document.getElementById('lay1').style.height = document.getElementById('hiper').clientHeight - altura + "px"
+    document.getElementById('lay1').style.top = `${altura}px`
+})
